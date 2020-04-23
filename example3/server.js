649d17ac26780f7132d5a30cf3d5a7078c5f24a3
Holzer <holzebrt@schaeffler.com>
2020-04-23 21:19:48 +0200

    add rooms

--- example3/server.js
+++ example3/server.js
@@ -1,25 +1,52 @@
 const NATS = require('nats');
-const  { v4 } = require('uuid');
+const { v4 } = require('uuid');
 const nc = NATS.connect({ json: true, servers: ['nats://localhost:5222'] });
 
 console.log("start server");
 
-let send = false;
+let rooms = {};
 
-nc.subscribe('newPlayer', function (msg) {
-    console.log("new player" + msg);
-    if (!send) {
-        setInterval(() => {
+nc.subscribe('newPlayer.>', (msg, reply, subject, sid) => {
 
-            x = Math.random() * (950 - 50) + 50
-            y = Math.random() * (650 - 50) + 50
-            r = Math.random() * (50 - 10) + 10
-            t = { id: v4(), x: x, y: y, r: r };
-            console.log("send new target");
-            nc.publish('newTarget', t)
-        }, 1000);
+    subs = subject.split('.');
+
+    if (!rooms.hasOwnProperty(subs[1])) {
+        rooms[subs[1]] = {
+            player: [],
+            starting: 10,
+            name: subs[1],
+            timer: {}
+        }
+
+        rooms[subs[1]].timer = setInterval((r, t) => {
+            nc.publish("startingIn." + r.name, r.starting);
+            r.starting = r.starting - 1;
+
+            if(r.starting === 0)
+            {
+                clearInterval(r.timer);
+                startGame(r);
+            }
+
+        }, 1000, rooms[subs[1]])
     }
+
+    rooms[subs[1]].player.push(msg);
+
+    console.log("new player" + subs);
+
 });
+
+function startGame(room) {
+    setInterval(() => {
+        x = Math.random() * (950 - 30) + 30
+        y = Math.random() * (650 - 30) + 30
+        r = Math.random() * (50 - 10) + 10
+        t = { id: v4(), x: x, y: y, r: r };
+        console.log("send new target");
+        nc.publish('newTarget.'+room.name, t)
+    }, 1000);
+}
 
 
 
